7b27063f8755bc3754828acc4236b3d96dca7006
rishi12438 <rishimahadevan@gmail.com>
2022-01-07 14:57:40 +0800

    added dropdown filler

--- content.js
+++ content.js
@@ -137,13 +137,69 @@ async function fill_workday(){
     }
     await sleep(2000)
   }
+  
+
   if(document.getElementsByClassName("css-1j9bnzb")[0].innerText == lastknown_title){
       return 
   }
   lastknown_title = document.getElementsByClassName("css-1j9bnzb")[0].innerText
- 
+  //this section is for the dropdown 
   $("div[data-automation-id*='formField']").each(function (i, el) {
-    console.log(el.children[0].innerText) 
+    var label_name  = el.children[0].innerText.replace("*","");
+    //this is the second div that contains the input 
+    var input_div = el.children[1]    
+
+    var to_search = sitesData["workday"]["selectorMapping"][label_name.toLowerCase()]
+    var value_to_look_for = ""
+    if(typeof to_search !== "undefined"){ 
+        value_to_look_for = autofillData[to_search]
+    }
+    found = 0 
+    //if button is present, means we have a dropdown 
+    if(typeof input_div !== "undefined" && input_div.getElementsByTagName("button").length > 0){ 
+      input_div.getElementsByTagName("button")[0].click()
+      document.getElementsByClassName('css-mq2y9k')[0].querySelector('[role="listbox"]')
+      options = document.getElementsByTagName('li')
+      i = 0 
+      while(i < options.length){
+        if(value_to_look_for != "" && typeof value_to_look_for !== "undefined" && options[i].innerText !== "undefined"  && value_to_look_for.toLowerCase() == options[i].innerText.toLowerCase()){ 
+          found = 1
+          options[i].click()
+        }        
+        //console.log(options[i].getAttribute('data-value'),options[i].innerText)
+        i++
+      }
+    }
+    else{ 
+
+    }
+    //close the dropdown 
+    if(found == 0){ 
+      if(typeof input_div !== "undefined" && input_div.getElementsByTagName("button").length > 0){ 
+        input_div.getElementsByTagName("button")[0].click()
+      }
+    }
+  });
+  await sleep(2000)
+  //console.log("called new",$("div[data-automation-id*='formField']") )
+  //this section is for the normal text input --> I have done it like this because when you do drop down more fields might be added
+  $("div[data-automation-id*='formField']").each(function (i, el) {
+    
+    var label_name  = el.children[0].innerText.replace("*","");
+    //this is the second div that contains the input 
+    var input_div = el.children[1]    
+
+    var to_search = sitesData["workday"]["selectorMapping"][label_name.toLowerCase()]
+    var value_to_look_for = ""
+    console.log(label_name,to_search,value_to_look_for)
+    if(typeof to_search !== "undefined"){ 
+        value_to_look_for = autofillData[to_search]
+    }
+    if(typeof input_div !== "undefined" && input_div.querySelectorAll('input[type=text]').length > 0 ){ 
+      if(value_to_look_for != "" && typeof value_to_look_for !== "undefined"){ 
+        input_div.querySelector('input[type=text]').value = value_to_look_for
+      }
+    }
   });
 
   if(done_next == 0){ 
